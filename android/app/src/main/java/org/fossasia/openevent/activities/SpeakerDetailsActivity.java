78d22599eb31e0c53253f7df57141ef322664b22
Mahika Wason <mahika16241@iiitd.ac.in>
2017-05-07 16:04:07 +0530

    Update SpeakerDetailsActivity.java

--- android/app/src/main/java/org/fossasia/openevent/activities/SpeakerDetailsActivity.java
+++ android/app/src/main/java/org/fossasia/openevent/activities/SpeakerDetailsActivity.java
@@ -25,7 +25,6 @@ import android.text.Html;
 import android.text.TextUtils;
 import android.text.method.LinkMovementMethod;
 import android.util.DisplayMetrics;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
